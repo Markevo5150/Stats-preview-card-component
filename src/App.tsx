bed656f2946c1596e1d75d5885dbd8ac29f8b6e5
Markevo5150 <marcoAlopez2@outlook.com>
2023-10-21 18:39:34 -0600

    First Docs

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="image"></div>
+        <div className="bottom">
+          <h1 className="title">
+            Get <span>insights</span> that help your business grow.
+          </h1>
+          <p className="info">
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency
+          </p>
+          <div className="stats">
+            <div className="stat">
+              <span className="number">10k+</span>
+              <span className="name">COMPANIES</span>
+            </div>
+
+            <div className="stat">
+              <span className="number">314</span>
+              <span className="name">TEMPLATES</span>
+            </div>
+
+            <div className="stat">
+              <span className="number">12M+</span>
+              <span className="name">QUERIES</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
